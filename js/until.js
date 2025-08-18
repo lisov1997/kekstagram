59d4060ba1212cab26af50cb3ad8d469c8034706
Sergey <serlisov1997@gmail.com>
2025-08-18 11:41:01 +0300

    3.1 Модуляция
    - разделил код, уже написанный в main.js, на модули. main.js -точка входа.
    - определил модули для дальнейшего кода

---
+++ js/until.js
@@ -0,0 +1,12 @@
+const getRandomPositiveInteger = (a, b = 1) => {
+  const lower = Math.ceil(Math.min(Math.abs(a),Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a),Math.abs(b)));
+  return Math.floor(Math.random()* (upper - lower + 1) + lower)
+}
+
+const checkStringLength = (string, length) => string.length <= length ;
+
+const getRandomArrayElement = (array) =>
+  array[getRandomPositiveInteger(0, array.length - 1)];
+
+export { getRandomPositiveInteger, checkStringLength, getRandomArrayElement };
