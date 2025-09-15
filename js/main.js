aa0856b15123684b5b4c2c7deefb837ad02161dd
Sergey <serlisov1997@gmail.com>
2025-09-15 14:07:50 +0300

    4.1 Манипуляции с DOM
    -Завел модуль, который будет отвечать за отрисовку миниатюр.
    
    -На основе временных данных для разработки и шаблона #picture создайте DOM-элементы, соответствующие фотографиям, и заполнил их данными.
    
    -Отрисовал сгенерированные DOM-элементы в блок .pictures. Для вставки элементов использовал DocumentFragment.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 import { getPhotos } from './data.js';
 
-getPhotos();
+const generatedPhotos = getPhotos();
+console.log(generatedPhotos);
+
+// На основе временных данных для разработки и шаблона #picture создайте DOM-элементы, соответствующие фотографиям, и заполните их данными:
+
+// {id: 1, url: 'photos/1}.jpg,', description: 'Так хочется обратно на этот пляж... 🏖️ #отпускмечты', likes: 92, comments: Array(8)}
+
+// Адрес изображения url подставьте как атрибут src изображения.
+
+// Количество лайков likes выведите в блок .picture__likes.
+
+// Количество комментариев comments выведите в блок .picture__comments.
+
+// Отрисуйте сгенерированные DOM-элементы в блок .pictures. Для вставки элементов используйте DocumentFragment.
+
+// Подключите модуль в проект.
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainerFragment = document.createDocumentFragment();
+
+generatedPhotos.forEach((url,likes,comments) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  picturesContainerFragment.appendChild(pictureElement);
+  console.group("Переменные");
+  console.log(pictureElement);
+  console.groupEnd();
+});
+
+picturesContainer.appendChild(picturesContainerFragment);
+
+
--- js/until.js
+++ js/until.js
@@ -1,8 +1,8 @@
 const getRandomPositiveInteger = (a, b = 1) => {
   const lower = Math.ceil(Math.min(Math.abs(a),Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a),Math.abs(b)));
-  return Math.floor(Math.random()* (upper - lower + 1) + lower)
-}
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
+};
 
 const checkStringLength = (string, length) => string.length <= length ;
 
